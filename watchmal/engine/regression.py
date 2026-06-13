Fix order of scaling if using both fixed scale and scale by PE

watchmal/engine/regression.py
@@ -88,12 +88,12 @@ class RegressionEngine(ReconstructionEngine):
         self.model_out = self.model(self.data)
         # split the output for each target
         split_model_out = torch.split(self.model_out, self.target_sizes, dim=1)
-        self.predictions = {"predicted_" + t: o * self.scale[t] + self.offset[t]
-                            for t, o in zip(self.target_key, split_model_out)}
         if self.scale_per_pe is not None:
             for t in self.scale_per_pe:
                 prediction = self.predictions["predicted_" + t]
                 prediction *= self.total_charge if prediction.dim() == 1 else self.total_charge[:, None]
+        self.predictions = {"predicted_" + t: o * self.scale[t] + self.offset[t]
+                            for t, o in zip(self.target_key, split_model_out)}
         if self.target_dict is None:
             return self.predictions
         return self.target_dict | self.predictions